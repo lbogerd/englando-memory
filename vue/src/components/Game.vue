958bd42ed9b66888a1e52f6f2b4f3ae142c56479
Lucas Bogerd <l.bogerd@protonmail.com>
2019-09-11 17:03:01 +0200

    Added first structure

---
+++ vue/src/components/Game.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="play-field">
+    <play-card
+      v-for="card in cards"
+      :key="card.id"
+      :card="card"
+      @click.native="clickCard(card)"
+    >
+
+    </play-card>
+  </div>
+</template>
+<script>
+import PlayCard from './PlayCard.vue'
+
+export default {
+  name: 'game',
+  components: {
+    PlayCard
+  },
+  data() {
+    return {
+      cards: [
+        {
+          id: 'asdf',
+          nl: 'muis',
+          isTextVisible: false
+        }
+      ]
+    }
+  },
+  methods: {
+    clickCard(card) {
+      if(card.isTextVisible){
+        return;
+      }
+
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
---
+++ vue/src/components/PlayCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div 
+    class="flip-card"
+  >
+    {{ card.nl }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    card: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    clickCard: function () {
+    }
+  }
+}
+</script>
