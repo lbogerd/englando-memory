95e144d545efc0f977fde0ff40f3fcc3a3c80ec2
Lucas Bogerd <l.bogerd@protonmail.com>
2019-09-18 23:24:24 +0200

    Removed useless styling added from the template

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -14,13 +14,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
